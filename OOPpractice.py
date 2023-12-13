111392bd2998af05399c7a09207c4d17486181ff
aceDavon <d.onate@simpsgroup.co.uk>
2023-12-13 11:22:40 +0100

    Implement Transfer method and extend the class to another
    - Implement the tranfer method on the accounts class and enable transfer
      from one account to the other,
    - Implement the interestAcct class and inherit all of the account data,
    - Overide the deposit and withdraw methods in the extended interestAcct class.

--- OOPpractice.py
+++ OOPpractice.py
@@ -1,5 +1,15 @@
 from accounts import *
 
 david = account(acctName="David", initial_deposit=1000)
+mary = account(acctName="mary", initial_deposit=2000)
+davon= interestAcct("Davon", 400)
 
 david.get_acct_balance()
+david.deposit(amount=820)
+davon.deposit(400)
+david.withdraw(4000)
+david.withdraw(400)
+david.transfer(2000, mary)
+david.transfer(200, mary)
+mary.get_acct_balance()
+davon.withdraw(100)
--- accounts.py
+++ accounts.py
@@ -1,10 +1,59 @@
-class is_viable_transaction(Exception):
-  pass
+class BalanceException(Exception):
+    pass
+
 
 class account():
-  def __init__(self, acctName, initial_deposit):
-    self.acctName = acctName
-    self.balance = initial_deposit
+    def __init__(self, acctName, initial_deposit):
+        self.acctName = acctName
+        self.balance = initial_deposit
 
-  def get_acct_balance(self):
-    print(f"\n {self.acctName}'s balance: ${self.balance:.2f}\n")
+    def is_viable_transaction(self, amount):
+        if amount >= self.balance:
+            raise BalanceException("Insufficient funds 🤕")
+
+    def get_acct_balance(self):
+        return (f"${self.balance:.2f}\n")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"\n Deposit successful.. \n {self.acctName}'s balance now: {
+              self.get_acct_balance()}")
+        
+    def withdraw(self, amount):
+        try:
+            self.is_viable_transaction(amount)
+            print(f"\n ******************** \n")
+            self.balance -= amount
+            print(f"\n Withdrawal Successful.. 🚀 \n {self.acctName}'s balance now: {self.get_acct_balance()}\n")
+            print(f"\n ******************** \n")
+        except BalanceException as error:
+            print(f" Withdrawal not successful: {error} \n")
+
+    def transfer(self, amount, acct):
+        try:
+            self.is_viable_transaction(amount)
+            print("********************************")
+            self.withdraw(amount)
+            acct.deposit(amount)
+            print(f"\n Transfer successful.. 🤙🏽 \n Yor Balance now: {self.get_acct_balance()}")
+            print("********************************")
+        except BalanceException as error:
+            print(f"Transfer Failed: {error}")
+
+class interestAcct(account):
+    def __init__(self, acctName, initial_deposit):
+        super().__init__(acctName, initial_deposit)
+        self.balance = initial_deposit * 1.05
+
+    def deposit(self, amount):
+        self.balance += amount * 1.05
+        print(f" Deposit successful.. \n {self.acctName}'s balance: {self.balance} \n")
+
+    def withdraw(self, amount):
+        try:
+            self.is_viable_transaction(amount * 1.05)
+            self.balance -= amount * 1.05
+            print(f"\n Withdrawal Successful.. 🚀 \n{self.acctName}'s balance now: {self.get_acct_balance()}\n")
+        except BalanceException as error:
+            print(f" withdrawal failed: {error}")
+
